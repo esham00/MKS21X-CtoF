498be029749c9d96d09c9f876ae784530b24e5ba
Elizabeth Sham <esham00@stuy.edu>
2018-09-19 00:12:35 -0400

    Added function for conversion from Fahrenheit to Celsius

---
+++ MKS21XCtoF.java
@@ -0,0 +1,8 @@
+public class MKS21XCtoF {
+  public static double celsiusToFahrenheit(double celsius) {
+    return celsius * 1.8 + 32;
+  }
+  public static double fahrenheitToCelsius(double fahrenheit) {
+    return (fahrenheit - 32) * (5/9);
+  }
+}
